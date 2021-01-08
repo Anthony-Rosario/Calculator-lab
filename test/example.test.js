e3b0d758fa0417ffd5d3e930ddef79526f11b05f
Anthony Rosario <anthonymrosario225@gmail.com>
2021-01-08 13:47:14 -0800

    added more tests

--- test/example.test.js
+++ test/example.test.js
@@ -2,6 +2,9 @@
 // import { example } from '../example.js';
 import { getAddSum, getSubSum, getDivSum, getMulSum } from '../utils.js';
 
+
+
+//Addition Tests
 const addTest = QUnit.test;
 
 addTest('you should see 12 displayed when inputing 8 and 4', (expect) => {
@@ -18,8 +21,18 @@ addTest('you should see 12 displayed when inputing 8 and 4', (expect) => {
     expect.equal(actual, expected);
 });
 
+addTest('someone should expect to see -166 after they input -123 and -43', (expect) => {
+    const expected = -166;
+
+    const actual = getAddSum(-123, -43);
+
+    expect.equal(actual, expected);
+});
 
 
+
+
+//Subtraction Tests
 const subTest = QUnit.test;
 
 subTest('someone should see 269 after inputing 312 and 43', (expect) => {
@@ -31,7 +44,17 @@ subTest('someone should see 269 after inputing 312 and 43', (expect) => {
 
 });
 
+subTest('when inputing 75 and 824 one should see -749', (expect) => {
+    const subExpected = -749;
 
+    const subActual = getSubSum(75, 824);
+
+    expect.equal(subActual, subExpected);
+});
+
+
+
+//Division tests
 const divTest = QUnit.test;
 
 divTest('if one does not see 45.25 after inputing 543 and 12 then something is seriously off', (expect) => {
@@ -42,8 +65,18 @@ divTest('if one does not see 45.25 after inputing 543 and 12 then something is s
     expect.equal(divActual, divExpected);
 });
 
+divTest('you should see -16 when someone gets fancy and inputs -32 and 2', (expect) => {
+    const divExpected = -16;
+
+    const divActual = getDivSum(-32, 2);
+
+    expect.equal(divActual, divExpected);
+});
 
 
+
+
+//Multiplication Tests
 const mulTest = QUnit.test;
 
 mulTest('if you dont see 82329214097412 after multiplying 94353426 by 872562 then it takes longer than that to teach a monkey how to kickflip', (expect) => {
@@ -54,3 +87,10 @@ mulTest('if you dont see 82329214097412 after multiplying 94353426 by 872562 the
     expect.equal(mulActual, mulExpected);
 });
 
+mulTest('if someone is seeing double and decided to input -90 and 90 one should see -8100', (expect) => {
+    const mulExpected = -8100;
+
+    const mulActual = getMulSum(-90, 90);
+
+    expect.equal(mulActual, mulExpected);
+});
